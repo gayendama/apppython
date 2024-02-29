399b657e4d77c0c7c318150639cf3f10a6ed5b45
gayendama <ndamagaye64@gmai.com>
2024-02-29 23:28:42 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def creer_liste_personnes():
+    personnes = [
+        {"nom": "ndama", "age": 25},
+        {"nom": "rochel", "age": 25},
+        {"nom": "fama", "age": 25},
+        {"nom": "fatoumata", "age": 25}
+    ]
+    return personnes
+
+#Exemple d'utilisation de la fonction
+liste_personnes = creer_liste_personnes()
+print(liste_personnes)
